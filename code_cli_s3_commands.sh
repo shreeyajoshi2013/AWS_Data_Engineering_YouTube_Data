f2856d258d81d3e0ee1eccdde173c62343249051
shreeyajoshi2013 <45735869+shreeyajoshi2013@users.noreply.github.com>
2023-01-12 23:45:20 -0500

    Upload aws cli code

---
+++ code_cli_s3_commands.sh
@@ -0,0 +1,16 @@
+# Command to load all JSON files from local to S3 Bucket folder all at once:
+
+aws s3 cp . s3://de-youtubedata-raw-useast1-dev/youtube/raw_statistics_reference_data/ --recursive --exclude "*" --include "*.json"
+
+# Commands to load CSV files from local to S3 Bucket in region-specific subfolders, following Hive-Style pattern:
+
+aws s3 cp CAvideos.csv s3://de-youtubedata-raw-useast1-dev/youtube/raw_statistics/region=ca/
+aws s3 cp DEvideos.csv s3://de-youtubedata-raw-useast1-dev/youtube/raw_statistics/region=de/
+aws s3 cp FRvideos.csv s3://de-youtubedata-raw-useast1-dev/youtube/raw_statistics/region=fr/
+aws s3 cp GBvideos.csv s3://de-youtubedata-raw-useast1-dev/youtube/raw_statistics/region=gb/
+aws s3 cp INvideos.csv s3://de-youtubedata-raw-useast1-dev/youtube/raw_statistics/region=in/
+aws s3 cp JPvideos.csv s3://de-youtubedata-raw-useast1-dev/youtube/raw_statistics/region=jp/
+aws s3 cp KRvideos.csv s3://de-youtubedata-raw-useast1-dev/youtube/raw_statistics/region=kr/
+aws s3 cp MXvideos.csv s3://de-youtubedata-raw-useast1-dev/youtube/raw_statistics/region=mx/
+aws s3 cp RUvideos.csv s3://de-youtubedata-raw-useast1-dev/youtube/raw_statistics/region=ru/
+aws s3 cp USvideos.csv s3://de-youtubedata-raw-useast1-dev/youtube/raw_statistics/region=us/
